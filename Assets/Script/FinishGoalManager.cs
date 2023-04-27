a9bdef4cc7a0035c39626ca6bda39dcf0beef7dc
SCYeon <duckgirl1123@gmail.com>
2023-04-28 00:43:58 +0900

    Stage2,3 Image Change

--- Assets/Script/FinishGoalManager.cs
+++ Assets/Script/FinishGoalManager.cs
@@ -5,9 +5,37 @@ using UnityEngine;
 public class FinishGoalManager : MonoBehaviour
 {
     CharacterData _dataManager;
+
+    bool _isUp = true;
+
+    float _move = 0;
+
     private void Awake()
     {
         _dataManager = GameObject.Find("Data").GetComponent<CharacterData>();
+    }
+
+    private void Update()
+    {
+        if(_move >= 50 && _isUp)
+        {
+            _isUp = false;
+        }
+        else if (_move <= -50 && !_isUp)
+        {
+            _isUp = true;
+        }
+        
+        if(_isUp)
+        {
+            _move += Time.timeScale;
+        }
+        else if (!_isUp)
+        {
+            _move -= Time.timeScale;
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y + (_move * 0.0005f), transform.position.z);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
